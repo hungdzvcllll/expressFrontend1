7ef1403f890bef1e073e036055d396f52ca172b7
unknown <fptjun@gmail.com>
2026-05-01 23:11:45 +0700

    fe

--- src/auth/protectedRoute.tsx
+++ src/auth/protectedRoute.tsx
@@ -1,13 +1,22 @@
 import React, { JSX } from "react";
 import { Navigate } from "react-router-dom";
 import { isTokenExpired,getUserRole } from "./auth";
-
+import { useLocation } from "react-router-dom";
 interface Props {
   children: JSX.Element;
   allowedRoles: string[];
 }
 
 const ProtectedRoute: React.FC<Props> = ({ children, allowedRoles }) => {
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const tokenFromUrl = params.get("token");
+
+  // 1. Nếu có token từ URL → lưu vào localStorage
+  if (tokenFromUrl) {
+    localStorage.setItem("token", tokenFromUrl);
+  }
+
   const token = localStorage.getItem("token");
   console.log(token);
   if (!token || isTokenExpired(token)) {
